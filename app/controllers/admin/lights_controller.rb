fee620d9c277e585be6d44c6c36d6553ea73e305
Steffen Witvliet <switvliet@rovecom.nl>
2017-11-30 13:17:24 +0100

    - setup controllers for admin

---
+++ app/controllers/admin/lights_controller.rb
@@ -0,0 +1,31 @@
+class Admin::LightsController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+
+  def light_params
+
+  end
+
+end
---
+++ app/controllers/admin/rooms_controller.rb
@@ -0,0 +1,32 @@
+class Admin::RoomsController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+
+  def room_params
+
+  end
+
+
+end
