01e7c0d6e29d4b9ce04421d19916e40c27eb80d2
Steffen Witvliet <switvliet@rovecom.nl>
2017-11-30 15:41:56 +0100

    - add swagger config - add swagger controller

---
+++ app/controllers/api/v1/lights_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::LightsController < ApplicationController
+  respond_to :json
+  swagger_controller :lights, "Light Management"
+
+  swagger_api :index do
+    summary "Fetches all Light items"
+    notes ""
+  end
+
+  def index
+    @lights = Light.all
+    render json: @lights, status: :ok
+  end
+
+end
